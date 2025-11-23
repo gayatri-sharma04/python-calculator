1e768606e100ab95a7270168c5d34fdb8591a4d8
gayatri-sharma04 <gayatri.sharma1520@gmail.com>
2025-11-23 23:26:51 +0545

    Remove unnecessary code

--- calculator.py
+++ calculator.py
@@ -9,12 +9,3 @@ b=int(input("Enter Second Number"))
 print("The sum of 2 number is",a+b)
 print("The sub of 2 number is", a-b)
 print("The div of two number is", a//b)
-
-'''
-
-word="developer"
-start=2
-end=len(word)-3
-print(word[start:end])
-'''
-
