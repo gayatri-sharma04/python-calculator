d93d080397daaac00d0ca2c940fd4d4e28eef2b7
gayatri-sharma04 <gayatri.sharma1520@gmail.com>
2025-11-23 23:20:37 +0545

    My first calculator

---
+++ calculator.py
@@ -0,0 +1,20 @@
+
+
+a=int(input("Enter First Number"))
+
+b=int(input("Enter Second Number"))
+
+#a=20
+#b=10
+print("The sum of 2 number is",a+b)
+print("The sub of 2 number is", a-b)
+print("The div of two number is", a//b)
+
+'''
+
+word="developer"
+start=2
+end=len(word)-3
+print(word[start:end])
+'''
+
