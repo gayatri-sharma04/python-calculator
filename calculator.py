ecd9b112bf21f60cf63d784bcebda348b418fa62
gayatri-sharma04 <gayatri.sharma1520@gmail.com>
2025-12-06 10:31:47 +0545

    removed old comments

--- calculator.py
+++ calculator.py
@@ -1,15 +1,4 @@
-'''
 
-a=int(input("Enter First Number"))
-
-b=int(input("Enter Second Number"))
-
-#a=20
-#b=10
-print("The sum of 2 number is",a+b)
-print("The sub of 2 number is", a-b)
-print("The div of two number is", a//b)
-'''
 class Calculator:
     def calculation(self,a,b,calc):
 	
